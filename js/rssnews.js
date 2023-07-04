71f58f473887cb2fa00e77d4ea0a57db599b4428
apamoni99 <primeoptimus5@outlook.com>
2023-07-04 23:25:23 +0800

    live rss news banjarmasin pos

--- js/rssnews.js
+++ js/rssnews.js
@@ -1,24 +1,27 @@
-// Fungsi untuk memuat data XML dari url
-    function loadXMLDoc(url) {
+    // read nodes value from xml file and set do array data wtih log
+    function readXMLToArray(xmlFile, tagname) {
       var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", url, false);
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+          // Ambil data XML
+          var xmlDoc = this.responseXML;
+          
+          //var element = document.getElementById("stypewrite");
+
+          // Ambil data tabel
+          var x = xmlDoc.getElementsByTagName(tagname);
+          var titleArray = [];
+          for (i = 0; i <x.length; i++) {
+            titleArray.push(x[i].childNodes[0].nodeValue);
+          }
+          //element.setAttribute("data-type", '['+titleArray+']');
+          console.log(titleArray);
+        }
+      };
+      xhttp.open("GET", xmlFile, true);
+      xhttp.setRequestHeader('Content-Type', 'text/xml');
       xhttp.send();
-      return xhttp.responseXML;
     }
 
-// Fungsi untuk menampilkan data XML ke tabel
-    function displayXML(url) {
-      var xml = loadXMLDoc(url); // Memuat data XML dari url
-      var table = document.getElementById("xml-table"); // Mendapatkan elemen tabel
-      var x = xml.getElementsByTagName("item"); // Mendapatkan elemen item dari data XML
-      for (var i = 0; i < x.length; i++) { // Melakukan iterasi untuk setiap item
-        var row = table.insertRow(i); // Menyisipkan baris baru ke tabel
-        var title = row.insertCell(0); // Menyisipkan sel baru untuk judul
-        var link = row.insertCell(1); // Menyisipkan sel baru untuk link
-        title.innerHTML = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue; // Mengisi sel judul dengan nilai dari elemen title
-        link.innerHTML = x[i].getElementsByTagName("link")[0].childNodes[0].nodeValue; // Mengisi sel link dengan nilai dari elemen link
-      }
-    }
-
-// Memanggil fungsi displayXML()
-    displayXML("https://www.w3schools.com/xml/note.xml");
+    readXMLToArray("../banjarmasin.tribunnews.com_rss.xml", "title");
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,3 +1,4 @@
+var titleArray = [];
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
@@ -39,15 +40,49 @@ var TxtType = function(el, toRotate, period) {
         }, delta);
     };
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
+    // read nodes value from xml file and set do array data wtih log
+    function readXMLToArray(xmlFile, tagname) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+          // Ambil data XML
+          var xmlDoc = this.responseXML;
+          
+          var element = document.getElementById("stypewrite");
+
+          // Ambil data tabel
+          var x = xmlDoc.getElementsByTagName(tagname);
+          //var titleArray = [];
+          for (i = 0; i <x.length; i++) {
+            titleArray.push(x[i].childNodes[0].nodeValue);
+          }
+          element.setAttribute("data-type", titleArray);
+          console.log(titleArray);
+        }
+      };
+      xhttp.open("GET", xmlFile, true);
+      xhttp.setRequestHeader('Content-Type', 'text/xml');
+      xhttp.send();
+    }
+    
+    try {
+        readXMLToArray("../banjarmasin.tribunnews.com_rss.xml", "title");
+    } catch (e) {
+        console.log(e);
+    } finally {
+        console.log("finally window.onload");
+        window.onload = function() {
+            var elements = document.getElementsByClassName('typewrite');
+            for (var i=0; i<elements.length; i++) {
+                var toRotate = elements[i].getAttribute('data-type');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                new TxtType(elements[i], titleArray, period);
+                }
             }
         }
+        
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
